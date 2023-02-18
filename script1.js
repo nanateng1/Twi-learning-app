af228752213c1cee8ad60392786b18012284f2ad
Nanateng <nanateng2020@gmail.com>
2023-02-18 02:41:14 +0000

    added modal to show total score

--- script1.js
+++ script1.js
@@ -44,12 +44,16 @@ let question = {
         });
       });
 
+      let blur = document.querySelector('.wrapper')
+      blur.classList.remove('blur')
+
       //refresh stats
-      this.updateStats();
+      // this.updateStats();
       
       // show first question
       this.showQuestion(questions[this.currPosition]);
 
+      //shows next question
       let next = document.getElementById('next')
       next.addEventListener('click', ()=>{
        this.showNextQuestion()
@@ -74,19 +78,28 @@ let question = {
     checkAnswer: function(userSelected) {
       
       let currQuestion = questions[this.currPosition];
+
+      let scoreDiv = document.getElementById('score')
+      let totalScore = document.querySelector('.total-score')
+
+      
+        
       
       if(currQuestion.correctAnswer == userSelected) {
         // correct
         this.score++;
         this.showResult(true)
         console.log(this.score);
+        scoreDiv.innerText = `Your score: ${this.score}`
+        totalScore.innerText = this.score
+        
       }
       else {
         this.showResult(false)
       }
 
       //refresh stats
-      this.updateStats()
+      // this.updateStats()
     },
 
     showNextQuestion: function(){
@@ -96,24 +109,31 @@ let question = {
             // show next question
             this.showQuestion(questions[this.currPosition]);
 
-            this.updateStats()
+            // this.updateStats()
     },
     
     increasePosition: function() {
       // increase the current position
       this.currPosition++;
-      
+   
       // if reached the end of the database
       if(this.currPosition == questions.length){
         // send back to the beginning
         this.currPosition = 0;
+        this.score = 0
+        this.gameEnd()
+       
       }
     },
 
-    updateStats: function() {
-        let scoreDiv = document.getElementById('score')
-        scoreDiv.textContext = `Your score: ${this.score}`
-    },
+    // updateStats: function() {
+    //     let scoreDiv = document.getElementById('score')
+    //     scoreDiv.textContext = `Your score: ${this.score}`
+    // },
+
+   
+
+    
 
     showResult: function(isCorrect) {
       //select DOM element
@@ -139,11 +159,42 @@ let question = {
       resultDiv.textContent = result
     },
 
+    gameEnd: function(){
+      
+      let blur = document.querySelector('.wrapper')
+      let showModal = document.querySelector('.modal')
+      let scoreDiv = document.getElementById('score')
+      let restart = document.querySelector('#play-again')
+
+      
+
+      //show modal and total score
+      this.increasePosition()
+      blur.classList.add('blur')
+      showModal.classList.remove('hidden')
+
+      // remove blur and start
+      restart.addEventListener('click',()=>{
+       blur.classList.remove('blur')
+        showModal.classList.add('hidden')
+        
+      })
+      console.log(this.checkAnswer);
+      this.currPosition = 0;
+      this.score = 0
+      
+      this.clearResult()
+      scoreDiv.innerText = ''
+
+    },
+    
+
     clearResult: function(){
       let resultDiv = document.getElementById('result')
 
       resultDiv.innerHTML = ""
     },
+    
     
     
   };
